d41b9501ebd01d7cc8f3b233b23195e01f9b1d53
GiacomoMilitello <g.militello95@gmail.com>
2024-01-11 16:34:45 +0100

    1-JSnack 3 js

--- blocco 1/JSnack 3/assets/js/main.js
+++ blocco 1/JSnack 3/assets/js/main.js
@@ -2,3 +2,24 @@
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 */
+
+const buttonOk = document.getElementById("ok");
+
+// Variabili ricavate da input
+var nOne = document.getElementById('n-one');
+var nTwo = document.getElementById('n-two');
+var nThree = document.getElementById('n-three');
+var nFour = document.getElementById('n-four');
+var nFive = document.getElementById('n-five');
+var nSix = document.getElementById('n-six');
+var nSeven = document.getElementById('n-seven');
+var nEight = document.getElementById('n-eight');
+var nNine = document.getElementById('n-nine');
+var nTen = document.getElementById('n-ten');
+
+buttonOk.addEventListener("click", function() {
+    var somma = Number(nOne.value) + Number(nTwo.value) + Number(nThree.value) + Number(nFour.value) + 
+                Number(nFive.value) + Number(nSix.value) + Number(nSeven.value) + Number(nEight.value) + 
+                Number(nNine.value) + Number(nTen.value);
+    document.getElementById("result").innerHTML = 'La somma è: ' + somma;
+});
